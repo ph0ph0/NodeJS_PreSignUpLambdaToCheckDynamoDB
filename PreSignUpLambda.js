be3ea4feaf6d3fe5c5f6e48947bd24366f94e2c3
ph0ph0 <phillipalanlee@googlemail.com>
2019-12-06 15:19:11 +0000

    Update PreSignUpLambda.js

--- PreSignUpLambda.js
+++ PreSignUpLambda.js
@@ -1,19 +1,17 @@
 const AWS = require("aws-sdk");
 
-const dynamo = new AWS.DynamoDB.DocumentClient({ region: `<Your-AWS-Region>` });
+const dynamo = new AWS.DynamoDB.DocumentClient({ region: `us-east-1` });
 
 exports.handler = async (event, context) => {
   console.log("Running presignup lambda...");
   console.log("processing event: %j", event);
 
-  const TableName = "<TableName>";
-  const IndexName = "<IndexName>";
-  
-  //You need to pass the value that you would like to check for in to the Lambda function via a custom attribute!
-  const userAttributes = event.request.userAttributes;
-  const username = userAttributes["custom:submittedUsername"];
+  const TableName = "User-or2wpavvmbatbhfn7vwoynk4c4-dev";
+  const IndexName = "findUsername";
+
+  const username = event.request.validationData.username
   console.log("username from event: " + username);
-  
+
   var params = {
     TableName: TableName,
     IndexName: IndexName,
